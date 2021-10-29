85db23cb613c462ce20e4bd07b46d9709fd823e5
Asy Syaffa Khoirunnisa <90151937+asysyaffak@users.noreply.github.com>
2021-10-30 01:50:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function tutuppopup(){
+    document.getElementById("parentpopup").style.display="none";
+}
+function bukapopup(){
+    document.getElementById("parentpopup").style.display="table";
+}
+function tutuppopup2(){
+    document.getElementById("parentpopup2").style.display="none";
+}
+function bukapopup2(){
+    document.getElementById("parentpopup2").style.display="table";
+}
+function tutuppopup3(){
+    document.getElementById("parentpopup3").style.display="none";
+}
+function bukapopup3(){
+    document.getElementById("parentpopup3").style.display="table";
+}
+function tutuppopup4(){
+    document.getElementById("parentpopup4").style.display="none";
+}
+function bukapopup4(){
+    document.getElementById("parentpopup4").style.display="table";
+}
+function tutuppopup5(){
+    document.getElementById("parentpopup5").style.display="none";
+}
+function bukapopup5(){
+    document.getElementById("parentpopup5").style.display="table";
+}
+function tutuppopup6(){
+    document.getElementById("parentpopup6").style.display="none";
+}
+function bukapopup6(){
+    document.getElementById("parentpopup6").style.display="table";
+}
+function tutuppopup7(){
+    document.getElementById("parentpopup7").style.display="none";
+}
+function bukapopup7(){
+    document.getElementById("parentpopup7").style.display="table";
+}
+function tutuppopup8(){
+    document.getElementById("parentpopup8").style.display="none";
+}
+function bukapopup8(){
+    document.getElementById("parentpopup8").style.display="table";
+}
+function tutuppopup9(){
+    document.getElementById("parentpopup9").style.display="none";
+}
+function bukapopup9(){
+    document.getElementById("parentpopup9").style.display="table";
+}
+function tutuppopup10(){
+    document.getElementById("parentpopup10").style.display="none";
+}
+function bukapopup10(){
+    document.getElementById("parentpopup10").style.display="table";
+}
